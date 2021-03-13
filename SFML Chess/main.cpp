ae703403f884b21deb181f4f4aeea53155898c0d
Andrew-Y-Xia <20240026@student.nths.net>
2021-03-13 08:34:08 -0600

    Initial Commit

---
+++ SFML Chess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SFML Chess
+//
+//  Created by Andy on 3/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
